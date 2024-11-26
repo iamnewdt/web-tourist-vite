290145095b0495e6abce3ff5d3932ff87ac56b49
iamnewdt <newtnc33@gmail.com>
2024-11-27 06:31:17 +0700

    27/11/2024 Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-   <footer>
-    <p>Copyright 2024</p>
-   </footer>
-        
-  )
+    <footer>
+      <div className="footer-container">
+        <div className="footer-content">
+          <p>&copy; 2024 Your Company. All rights reserved.</p>
+          <ul className="footer-menu">
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <div className="footer-social">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
